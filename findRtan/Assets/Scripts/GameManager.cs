01d2fdd9f6b5f5f0b33b91205e0c8458f235b2e6
junzerokim <junzerokim@gmail.com>
2023-08-26 23:43:43 +0900

    :memo: Add: findRtan Animations

--- findRtan/Assets/Scripts/GameManager.cs
+++ findRtan/Assets/Scripts/GameManager.cs
@@ -2,15 +2,25 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using System.Linq;
 
 public class GameManager : MonoBehaviour
 {
     public Text timeText;
     public GameObject card;
     float time;
+    public static GameManager I;
+    
+    void Awake()
+    {
+        I = this;
+    }
     // Start is called before the first frame update
     void Start()
     {
+        int[] rtans = { 0, 0, 1, 1, 2, 2, 3, 3, 4, 4, 5, 5, 6, 6, 7, 7 };
+        rtans = rtans.OrderBy(item => Random.Range(-1.0f, 1.0f)).ToArray();
+        
         for (int i = 0; i < 16; i++)
         {
             GameObject newCard = Instantiate(card);
@@ -20,6 +30,9 @@ public class GameManager : MonoBehaviour
             float x = (i / 4) * 1.4f - 2.1f;
             float y = (i % 4) * 1.4f - 3.0f;
             newCard.transform.position = new Vector3(x, y, 0);
+
+            string rtanName = "rtan" + rtans[i].ToString();
+            newCard.transform.Find("front").GetComponent<SpriteRenderer>().sprite = Resources.Load<Sprite>(rtanName);
         }
     }
 
---
+++ findRtan/Assets/Scripts/card.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class card : MonoBehaviour
+{
+    public Animator anim;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void open()
+    {
+        anim.SetBool("isOpen", true);
+        transform.Find("front").gameObject.SetActive(true);
+        transform.Find("back").gameObject.SetActive(false);
+    }   
+}
