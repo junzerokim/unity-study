3da7a1bb7eddb73790bba92f75eae735b7046bbf
junzerokim <junzerokim@gmail.com>
2023-11-15 00:08:23 +0900

    :dizzy: Add: Panda Run Background

--- pandaRun/Assets/Scripts/BackgroundScroll.cs
+++ pandaRun/Assets/Scripts/BackgroundScroll.cs
@@ -4,16 +4,22 @@ using UnityEngine;
 
 public class BackgroundScroll : MonoBehaviour
 {
+    [SerializeField]
+    private float moveSpeed = 1f;
     private float width;
     // Start is called before the first frame update
     void Start()
     {
-        
+        SpriteRenderer renderer = GetComponent<SpriteRenderer>();
+        width = renderer.sprite.bounds.size.x;
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        transform.position += Vector3.left * moveSpeed * Time.deltaTime;
+        if (transform.position.x <= -width) {
+            transform.position += new Vector3(width * 2f, 0, 0);
+        }
     }
 }
