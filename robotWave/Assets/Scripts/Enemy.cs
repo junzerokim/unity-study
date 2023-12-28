5266e66c17137cf349cdf17402324a1ae33d8553
junzerokim <junzerokim@gmail.com>
2023-12-28 23:05:49 +0900

    :dizzy: Add: Enemy Direction

--- robotWave/Assets/Scripts/Enemy.cs
+++ robotWave/Assets/Scripts/Enemy.cs
@@ -21,5 +21,17 @@ public class Enemy : MonoBehaviour
     {
         Vector3 moveTo = (targetTransform.position - transform.position).normalized;
         transform.position += moveTo * moveSpeed * Time.deltaTime;
+
+        Vector3 currScale = transform.localScale;
+        Vector3 currHpBarScale = hpBar.transform.localScale;
+        if (moveTo.x > 0) {
+            transform.localScale = new Vector3(-Mathf.Abs(currScale.x), currScale.y, currScale.z);
+            hpBar.transform.localScale = new Vector3(-Mathf.Abs(currHpBarScale.x), currHpBarScale.y, currHpBarScale.z);
+        } else {
+            transform.localScale = new Vector3(Mathf.Abs(currScale.x), currScale.y, currScale.z);
+            hpBar.transform.localScale = new Vector3(Mathf.Abs(currHpBarScale.x), currHpBarScale.y, currHpBarScale.z);
+        }
+
+        hpBar.SetHP(2f, 10f);
     }
 }
